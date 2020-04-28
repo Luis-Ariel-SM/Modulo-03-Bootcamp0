d6146bb6f990e5126b525023187e13458eb7882d
Luis Ariel Santander Méndez <luisarielsantander@gmail.com>
2020-04-28 20:45:44 +0200

    cartas 1era version completa

--- P01 Cartas inicio.py
+++ P01 Cartas inicio.py
@@ -5,3 +5,10 @@ b2 = (P01_ModuloCartas.crea_baraja())
 
 print (b1)
 print (b2)
+
+print (P01_ModuloCartas.mezclar_baraja(b1))
+
+b = P01_ModuloCartas.crea_baraja()
+manos = P01_ModuloCartas.repartir(b,3,5)
+
+print(manos)
--- P01_ModuloCartas.py
+++ P01_ModuloCartas.py
@@ -1,3 +1,5 @@
+import random
+
 palos = ('O','C','E','B')
 cartas = ('A','2','3','4','5','6','7','S','C','R')
 
@@ -11,3 +13,27 @@ def crea_baraja ():
             baraja.append (naipe)
 
     return (baraja)
+
+def mezclar_baraja (baraja):
+    br = []
+    i = 0
+    while i < 40:
+        n = random.randint(0,39)
+        while baraja[n] in br:
+            n = random.randint(0,39)
+        br.append (baraja[n])
+        i += 1
+
+    b = br
+    return b  
+
+def repartir (b, jugadores, cartas):
+    res = []
+    for p in range (jugadores):
+        res.append([])
+    for ic in range (cartas):
+        for ij in range (jugadores):
+            carta = b.pop(0)
+            res[ij].append(carta)
+    return res   
+    
